ebc3391f0f4937b9138d690c6eb8e7599f58e9aa
KasjanK <kowalczykkasjan@gmail.com>
2024-12-27 11:29:22 +0100

    added character count and a finalized report

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 def main(): 
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        wordcount = get_wordcount(file_contents)
-        print(wordcount)
+        report = get_report(file_contents)
+        print(report)
+        
     
 def get_wordcount(text):
     word_list = text.split()
     return len(word_list)
 
+def get_charcount(text):
+   letter_dict = {}
+   lowered_text = text.lower()
+   for char in lowered_text:
+      letter_dict[char] = letter_dict.get(char, 0) + 1
+   return letter_dict
+
+def get_report(text):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{get_wordcount(text)} words found in the document")
+    print("")
+    charcount = get_charcount(text)
+    char_list = []
+   
+    for char, count in charcount.items():
+        if char.isalpha():
+            char_list.append({"character": char, "num": count})     
+
+    char_list.sort(reverse=True, key=sort_on)  
+    for char in char_list:
+        print(f"The '{char["character"]}' character was found {char["num"]} times")
+    print("--- End report ---")
+
+def sort_on(dict):
+    return dict["num"]
+   
 main()
